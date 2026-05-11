70f295501247ae55c53fe6845ca3e1faa8ab1e34
maver <git.escargot761@passinbox.com>
2026-05-12 00:16:14 +0200

    Add missing market person show command

---
+++ src/main/kotlin/maver/talkingonstations/command/MarketPersonShowCommand.kt
@@ -0,0 +1,55 @@
+package maver.talkingonstations.command
+
+import com.fs.starfarer.api.Global
+import maver.talkingonstations.TosRegistry
+import maver.talkingonstations.llm.LLMContext
+import maver.talkingonstations.llm.dto.GameInfo
+import org.lazywizard.console.BaseCommand
+import org.lazywizard.console.Console
+
+class MarketPersonShowCommand : BaseCommand {
+    override fun runCommand(
+        p0: String,
+        p1: BaseCommand.CommandContext
+    ): BaseCommand.CommandResult? {
+        if (p0.isEmpty()) return BaseCommand.CommandResult.BAD_SYNTAX
+
+        val args = p0.trim().split(Regex("\\s+"))
+        val id = args.first()
+        val toLog = args.getOrNull(1)
+
+        val entry = TosRegistry.getMarketPersons().entries
+            .find { (_, data) -> data.id == id }
+        if (entry == null) {
+            Console.showMessage("Market person $id not found")
+            return BaseCommand.CommandResult.SUCCESS
+        }
+
+        val (person, data) = entry
+        val market = person.market
+            ?: Global.getSector().economy.getMarket(data.market)
+        if (market == null) {
+            Console.showMessage("Market ${data.market} not found")
+            return BaseCommand.CommandResult.SUCCESS
+        }
+
+        val context = LLMContext(
+            GameInfo(
+                Global.getSector().playerPerson,
+                person,
+                market,
+            )
+        )
+
+        if (toLog != null) {
+            Global.getLogger(javaClass).info("Context for ${person.name.fullName} at ${market.name}:\n${context.getSystemInstructionsMerged()}")
+            Console.showMessage("Dumped context for ${person.name.fullName} to starsector.log")
+        } else {
+            Console.showMessage("Dumping context for ${person.name.fullName} at ${market.name}")
+            Console.showMessage(context.getSystemInstructionsMerged())
+            Console.showMessage("\n")
+        }
+
+        return BaseCommand.CommandResult.SUCCESS
+    }
+}
