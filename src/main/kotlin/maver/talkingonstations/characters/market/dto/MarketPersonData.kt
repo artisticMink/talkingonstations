dadc43aac4744a296eb03beac0464767a48f7ebe
maver <git.escargot761@passinbox.com>
2026-05-12 00:09:44 +0200

    Refactor market person data

--- src/main/kotlin/maver/talkingonstations/characters/market/dto/MarketPersonData.kt
+++ src/main/kotlin/maver/talkingonstations/characters/market/dto/MarketPersonData.kt
@@ -1,17 +1,24 @@
 package maver.talkingonstations.characters.market.dto
 
 import com.fs.starfarer.api.characters.FullName
+import maver.talkingonstations.characters.market.MarketPersonInterface
 
+/**
+ * Represents a row in MarketPerson.csv
+ * @see /data/config/tos/MarketPerson.csv
+ */
 data class MarketPersonData(
+    val id: String,
     val gender: FullName.Gender,
     val name: String,
-    val surename: String,
+    val surname: String,
     val faction: String,
     val market: String,
-    val rank: String?,
-    val post: String?,
+    val rank: String,
+    val post: String,
     val portrait: String,
-    val voice: String?,
-    val background: String?,
-    val knowledgeBlacklist: String?
+    val voice: String,
+    val background: String,
+    val knowledgeBlacklist: String,
+    val personExtension: MarketPersonInterface?
 )
--- src/main/kotlin/maver/talkingonstations/characters/market/dto/PersonExtensionData.kt
+++
@@ -1,8 +0,0 @@
-package maver.talkingonstations.characters.market.dto
-
-import maver.talkingonstations.llm.ContextMixinInterface
-
-data class PersonExtensionData(
-    val background: String = "",
-    val knowledgeBlacklist: List<ContextMixinInterface> = listOf()
-)
