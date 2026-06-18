9e3c6a6e917c1e523ee27953a7f06874b7747551
maver <git.escargot761@passinbox.com>
2026-06-18 21:31:41 +0200

    Fix stale frame script queue
    And add ability to all characters on load

--- src/main/kotlin/maver/talkingonstations/plugins/TalkingOnStationsPlugin.kt
+++ src/main/kotlin/maver/talkingonstations/plugins/TalkingOnStationsPlugin.kt
@@ -1,6 +1,7 @@
 package maver.talkingonstations.plugins
 
 import com.fs.starfarer.api.BaseModPlugin
+import com.fs.starfarer.api.Global
 import com.fs.starfarer.api.impl.campaign.intel.bar.events.BarEventManager
 import maver.talkingonstations.TosEveryFrameScriptQueue
 import maver.talkingonstations.TosRegistry
@@ -11,21 +12,25 @@ internal class TalkingOnStationsPlugin : BaseModPlugin() {
     override fun onApplicationLoad() {
         LunaSettingsRegistration.register()
         TosEveryFrameScriptQueue.setup()
-
     }
 
     override fun onGameLoad(newGame: Boolean) {
         super.onGameLoad(newGame)
 
-        // Load csv, json, etc.
-        if (!TosRegistry.isInitialized()) {
-            TosRegistry.initialize()
-        }
+        TosEveryFrameScriptQueue.setup()
+        TosRegistry.initialize()
 
         val barEventManager = BarEventManager.getInstance()
         if (!barEventManager.hasEventCreator(ChatBarEventWithPersonCreator::class.java)) {
             // The default random person bar event, available whenever a player visits a market.
             barEventManager.addEventCreator(ChatBarEventWithPersonCreator())
         }
+
+        // Add ability to view the player profile
+        val characterData = Global.getSector().characterData
+        if (!characterData.abilities.contains("tos_opentrichat")) {
+            characterData.addAbility("tos_opentrichat")
+        }
+
     }
 }
